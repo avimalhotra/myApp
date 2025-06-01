376c7ac29d49d68a266aaaeebda5334bdd94b99d
Avinash <avimalhotra505@gmail.com>
2025-06-01 12:40:13 +0530

    Reactive Form with Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 // import { Login } from './login';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormControl, FormArray, FormGroup } from '@angular/forms';
+import { FormControl, FormArray, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 
 
@@ -27,20 +27,28 @@ export class AppComponent {
   // email=new FormControl("");
   // pass=new FormControl("");
 
+ 
+
   login=new FormGroup({
-    name:new FormControl(""),
-    email:new FormControl(""),
-    pass:new FormControl("")
+    name:new FormControl("",[Validators.required, Validators.minLength(3)]),
+    
+    email:new FormControl("",[Validators.required,Validators.minLength(6),Validators.pattern(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
+
+    pass:new FormControl("",[Validators.required,Validators.minLength(6),Validators.maxLength(12),Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/)])
   });
+
+
+
 
   submitForm(){
     console.log(this.login.value);
     console.log(this.login.status);
     console.log(this.login.valid);
-    console.log(this.login.invalid);
-
+    // console.log(this.login.invalid);
   }
 
+
+ 
 
 
  
