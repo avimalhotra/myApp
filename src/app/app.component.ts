3dccd36604deef8354f99dfbc8c72681942143f0
Avinash <avimalhotra505@gmail.com>
2025-06-07 12:20:18 +0530

    View Encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 // import { RevsortPipe } from './revsort.pipe';
@@ -7,15 +7,17 @@ import { FormsModule } from '@angular/forms';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormControl, FormArray, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { CoursesComponent } from "./courses/courses.component";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, FormsModule, ReactiveFormsModule ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, CoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  preserveWhitespaces:true
+  preserveWhitespaces:true,
+  encapsulation:ViewEncapsulation.None
 })
 
 export class AppComponent {
@@ -35,9 +37,8 @@ export class AppComponent {
     email:new FormControl("",[Validators.required,Validators.minLength(6),Validators.pattern(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
 
     pass:new FormControl("",[Validators.required,Validators.minLength(6),Validators.maxLength(12),Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/)])
+
   });
-
-
 
 
   submitForm(){
@@ -45,6 +46,15 @@ export class AppComponent {
     console.log(this.login.status);
     console.log(this.login.valid);
     // console.log(this.login.invalid);
+  }
+
+
+  setVal(){    
+    this.login.setValue({'name':"aaa",'email':"a@b.cc",'pass':'Aa123456'})
+  }
+
+  patchval(){
+    this.login.patchValue({email:"lorem@ipsum.com"})
   }
 
 
